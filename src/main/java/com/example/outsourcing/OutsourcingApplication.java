2692f5fdfff9416c41e6a805d5ca7a9f484a3ca7
hojin <hojin915@naver.com>
2025-06-13 12:18:36 +0900

    init

---
+++ src/main/java/com/example/outsourcing/OutsourcingApplication.java
@@ -0,0 +1,12 @@
+package com.example.outsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsourcingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OutsourcingApplication.class, args);
+    }
+
+}
