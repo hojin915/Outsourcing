1b5d0cd2bc98b2303be9bbfeacf8e3c8af18f238
ryusu12 <rsy7567@gmail.com>
2025-06-15 18:06:53 +0900

    feat: Security와 jwt 적용
    UserRole 생성
    - common/enums/UserRole.java
    - ADMIN, USER
    
    에러 응답 형식 정의
    - common/dto/ErrorResponseDto.java
    
    현재 Security는 모든 api 요청을 인증없이 허용함
    - 실행전 SECRET_KEY 설정 필수
    - 인증, 인가 예외 처리 진행함
    - security에서 제공하는 User 객체를 사용하여 SecurityContext에 인증 토큰 저장함

---
+++ src/main/java/com/example/outsourcing/common/config/JwtUtil.java
@@ -0,0 +1,75 @@
+package com.example.outsourcing.common.config;
+
+import com.example.outsourcing.common.enums.UserRole;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.security.auth.message.AuthException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Slf4j(topic = "JwtUtil")
+@Component
+public class JwtUtil {
+
+    private static final String BEARER_PREFIX = "Bearer ";
+    private static final long TOKEN_TIME = 30 * 60 * 1000L; // 30분
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    private Key key;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String createToken(Long userId, String username, UserRole userRole) {
+        Date date = new Date();
+
+        return BEARER_PREFIX + Jwts.builder()
+                .setSubject(String.valueOf(userId))
+                .claim("username", username)
+                .claim("userRole", userRole)
+                .setExpiration(new Date(date.getTime() + TOKEN_TIME))
+                .setIssuedAt(date)
+                .signWith(key, signatureAlgorithm)
+                .compact();
+    }
+
+    // JWT 반환
+    public String substringToken(String tokenValue) throws AuthException {
+        if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
+            return tokenValue.substring(7);
+        }
+        throw new AuthException();
+    }
+
+    // JWT 에서 페이로드 반환
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return extractClaims(token).get("username", String.class);
+    }
+
+    public UserRole getUserRoleFromToken(String token) {
+        return UserRole.of(extractClaims(token).get("userRole", String.class));
+    }
+
+}
