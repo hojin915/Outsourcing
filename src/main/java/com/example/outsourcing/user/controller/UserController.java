ecf78c5a8a9577b7a82e2fc2dfb94b0e9abf9788
ryusu12 <rsy7567@gmail.com>
2025-06-18 15:24:53 +0900

    fix: 회원가입, 로그인 api 변경 (/api/users -> /api/auth) 및 회원탈퇴 api 변경
    프론트에서 요구하는 회원가입, 로그인 api가 users가 아닌 auth로 요구되고 있었음
    - 회원탈퇴의 경우 POST /api/auth/withdraw 를 요구함에 따라 변경

--- src/main/java/com/example/outsourcing/user/controller/UserController.java
+++ src/main/java/com/example/outsourcing/user/controller/UserController.java
@@ -7,7 +7,6 @@ import com.example.outsourcing.user.dto.request.UserSignupRequestDto;
 import com.example.outsourcing.user.dto.response.UserLoginResponseDto;
 import com.example.outsourcing.user.dto.response.UserProfileResponseDto;
 import com.example.outsourcing.user.dto.response.UserSignupResponseDto;
-import com.example.outsourcing.user.entity.User;
 import com.example.outsourcing.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,17 +17,17 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
-// Front URL 매칭 필요
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
+
     private final UserService userService;
 
-    @PostMapping("/register")
+    @PostMapping("/auth/register")
     public ResponseEntity<ResponseDto<UserSignupResponseDto>> signup(
             @Valid @RequestBody UserSignupRequestDto requestDto
-    ){
+    ) {
         UserSignupResponseDto responseDto = userService.signup(requestDto);
         URI location = URI.create("/api/users/" + responseDto.getId());
 
@@ -36,32 +35,33 @@ public class UserController {
         return ResponseEntity.created(location).body(response);
     }
 
-    @PostMapping("/login")
+    @PostMapping("/auth/login")
     public ResponseEntity<ResponseDto<UserLoginResponseDto>> login(
             @Valid @RequestBody UserLoginRequestDto requestDto
-    ){
+    ) {
         UserLoginResponseDto responseDto = userService.login(requestDto);
 
         ResponseDto<UserLoginResponseDto> response = new ResponseDto<>("로그인이 완료되었습니다.", responseDto);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/me")
+    @GetMapping("/users/me")
     public ResponseEntity<ResponseDto<UserProfileResponseDto>> profile(
             @AuthenticationPrincipal(expression = "username") String username
-    ){
+    ) {
         UserProfileResponseDto responseDto = userService.getProfile(username);
-        ResponseDto<UserProfileResponseDto> response = new ResponseDto<>("프로필 조회에 성공했습니다.", responseDto);
 
+        ResponseDto<UserProfileResponseDto> response = new ResponseDto<>("프로필 조회에 성공했습니다.", responseDto);
         return ResponseEntity.ok(response);
     }
 
-    @DeleteMapping
+    @PostMapping("/auth/withdraw")
     public ResponseEntity<ResponseDto<Void>> delete(
             @AuthenticationPrincipal(expression = "username") String username,
             @RequestBody UserDeleteRequestDto request
-    ){
+    ) {
         userService.delete(username, request);
+
         ResponseDto<Void> response = new ResponseDto<>("회원탈퇴가 완료되었습니다", null);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
